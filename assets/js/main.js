c01db18249cba62634c18f36fec261aa6453143a
alessiaBer <alexdeyn@gmail.com>
2023-03-10 15:09:34 +0100

    [move] variables into for loop to create each li

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,26 +3,6 @@ per i multipli di 3 stampi “Fizz” al posto del numero
 per i multipli di 5 stampi “Buzz”
 per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” */
 
-//creo un for loop che stampi nella console i numeri da 1 a 100
-for (let i = 1; i <= 100; i++) {
-    
-    //dichiaro un if statement per definire cosa loggare nella console nel caso:
-    if (i % 3 === 0 && i % 5 === 0) {
-        // il numero sia un multiplo sia di 3 che di 5
-        console.log('FizzBuzz');
-    } else if ( i % 3 === 0) {
-        //il numero sia un numero solo di 3
-        console.log('Fizz');
-    } else if (i % 5 === 0) {
-        // il numero sia un multiplo solo di 5
-        console.log('Buzz');
-    } else {
-        // il numero non rispecchia nessuno dei casi precedenti
-        console.log(i);
-    }
-
-}
-
 /* Crea un container nel DOM , 
 aggiungendo un elemento html con il numero o la stringa corretta da mostrare.
 Potete usare vari strumenti per farlo:
@@ -36,7 +16,30 @@ const container = document.querySelector('.container');
 const ulElement = document.createElement('ul');
 //aggiungo al container la variabile dell'elemento ul 
 container.append(ulElement);
-//creo un li element e lo assegno ad una variabile
-const liElement = document.createElement('li');
-//aggiungo all'elemento ul un elemento li per ogni numero creato nel for loop
-ulElement.append(liElement);
+
+
+//creo un for loop che stampi nella console i numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    
+    //creo un li element e lo assegno ad una variabile
+    const liElement = document.createElement('li');
+    //aggiungo all'elemento ul un elemento li per ogni numero creato nel for loop
+    ulElement.append(liElement);
+
+    //dichiaro un if statement per definire cosa loggare nella console nel caso:
+    if (i % 3 === 0 && i % 5 === 0) {
+        // il numero sia un multiplo sia di 3 che di 5
+        liElement.append('FizzBuzz');
+    } else if ( i % 3 === 0) {
+        //il numero sia un numero solo di 3
+        liElement.append('Fizz');
+    } else if (i % 5 === 0) {
+        // il numero sia un multiplo solo di 5
+        liElement.append('Buzz');
+    } else {
+        // il numero non rispecchia nessuno dei casi precedenti
+        liElement.append(i);
+    }
+
+}
+
