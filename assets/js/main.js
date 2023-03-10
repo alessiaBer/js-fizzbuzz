184049fedeaa5ac45ffc297fee3bfa544b38fd89
alessiaBer <alexdeyn@gmail.com>
2023-03-10 14:01:47 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+/* Scrivi un programma che stampi in console i numeri da 1 a 100 ma che:
+per i multipli di 3 stampi “Fizz” al posto del numero
+per i multipli di 5 stampi “Buzz”
+per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” */
+
+//creo un for loop che stampi nella console i numeri da 1 a 100
+
+//dichiaro un if statement per dichiarare l'innerHTML nel caso
+
+//il numero sia un multiplo di 3
+
+//il numero sia un multiplo di 5
+
+//il numero sia un multiplo sia di 3 che di 5
