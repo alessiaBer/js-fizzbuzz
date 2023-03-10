9af33983da9a2f69e747f21d7249755cbdda9fc8
alessiaBer <alexdeyn@gmail.com>
2023-03-10 14:48:49 +0100

    [add] for loop to console.log numbers or strings

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,21 @@ per i multipli di 5 stampi “Buzz”
 per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” */
 
 //creo un for loop che stampi nella console i numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    
+    //dichiaro un if statement per definire cosa loggare nella console nel caso:
+    if (i % 3 === 0 && i % 5 === 0) {
+        // il numero sia un multiplo sia di 3 che di 5
+        console.log('FizzBuzz');
+    } else if ( i % 3 === 0) {
+        //il numero sia un numero solo di 3
+        console.log('Fizz');
+    } else if (i % 5 === 0) {
+        // il numero sia un multiplo solo di 5
+        console.log('Buzz');
+    } else {
+        // il numero non rispecchia nessuno dei casi precedenti
+        console.log(i);
+    }
 
-//dichiaro un if statement per dichiarare l'innerHTML nel caso
-
-//il numero sia un multiplo di 3
-
-//il numero sia un multiplo di 5
-
-//il numero sia un multiplo sia di 3 che di 5
+}
